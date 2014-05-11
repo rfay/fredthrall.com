3a4efcde5684350f15b99450c956c009e3175b51
Randy Fay <randy@randyfay.com>
2014-05-11 10:28:54 -0600

    Remove useless D6 modules (uninstalled)

--- sites/all/modules/thickbox/thickbox_login.js
+++
@@ -1,6 +0,0 @@
-// $Id: thickbox_login.js,v 1.2.2.2 2008/09/06 19:27:21 frjo Exp $
-// Contributed by user jmiccolis.
-Drupal.behaviors.initThickboxLogin = function(context) {
-  $(document).ready(function() { $("a[@href*='/user/login']").addClass('thickbox').each(function() { this.href = this.href.replace(/user\/login\??/,"user/login/thickbox?height=230&width=250&") }) });
-  $(document).ready(function() { $("a[@href*='?q=user/login']").addClass('thickbox').each(function() { this.href = this.href.replace(/user\/login/,"user/login/thickbox&height=230&width=250") }) });
-}
